f43027ac5273ffc12b8974930d867e7212f9d61a
Gabe Rojas-Westall <rojaswestall@gmail.com>
2018-10-19 02:21:48 -0500

    added form on button click

--- client/src/AddMeal.js
+++ client/src/AddMeal.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button } from 'semantic-ui-react'
+import { Button, Form } from 'semantic-ui-react'
 
 const lowerNavCss = {
   margin: "auto",
@@ -24,7 +24,27 @@ class AddMeal extends Component {
 
   showForm() {
     if (this.state.clicked) {
-      return (<div> Yay </div>);
+      return (
+        <Form>
+          <Form.Field>
+            <label>Name of Meal</label>
+            <input placeholder='Name of Meal' />
+          </Form.Field>
+          <Form.Field>
+            <label>Meals Available</label>
+            <input placeholder='# of Meals Available' />
+          </Form.Field>
+          <Form.Field>
+            <label>Ingredients</label>
+            <input placeholder='Ingredients' />
+          </Form.Field>
+          <Form.Field>
+            <label>Description</label>
+            <input placeholder='Description' />
+          </Form.Field>
+          <Button type='submit'>Submit</Button>
+        </Form>
+      );
     }
   }
 
