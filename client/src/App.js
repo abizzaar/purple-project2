495be4e122175c1e46703eda5283cf2c37e2a172
Abizar Bagasrawala <babyzar97@gmail.com>
2018-10-19 02:07:41 -0500

    workin

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,31 @@ import Nav from './Nav.js'
 import Posts from './Posts.js'
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      author: '',
+      name: '',
+      description: '',
+      number: '',
+    };
+  }
+
+  componentDidMount() {
+
+  }
+
+  loadPostsFromServer = () => {
+    // fetch returns a promise. If you are not familiar with promises, see
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
+    fetch('/api/posts/')
+      .then(data => data.json())
+      .then((res) => {
+        if (!res.success) this.setState({ error: res.error });
+        else this.setState({ data: res.data });
+      });
+  }
+
   render() {
     return (
       <div>
