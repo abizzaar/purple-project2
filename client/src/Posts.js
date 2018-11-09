0ee512c0e5d8c72fde1c9bb8835b067362468d86
omkarstpt <omkarstpt@gmail.com>
2018-11-08 20:38:51 -0600

    Now making posts disappear if available meals are 0

--- client/src/Posts.js
+++ client/src/Posts.js
@@ -1,47 +1,42 @@
-import React, {Component} from 'react';
-import { Card } from 'semantic-ui-react'
-import LikeName from './LikeName.js'
+import React, { Component } from "react";
+import { Card } from "semantic-ui-react";
+import LikeName from "./LikeName.js";
 
 const lowerNavCss = {
   margin: "auto",
   marginTop: "2rem"
-}
-
+};
 
 const header = {
   textTransform: "uppercase"
-}
+};
 
-
-class Post extends Component { 
+class Post extends Component {
   constructor() {
     super();
-    this.state = {
-    }
+    this.state = {};
   }
 
   render() {
     let posts = [];
     for (let post of this.props.posts) {
-      posts.push(
-        <div  style={lowerNavCss} class='ui card'>
-        <div class='content'>
-          <div style={header} class='header'>{post.name}</div>
-          <div class='meta'>&#8226; by {post.author} </div>
-          <div class='meta'>&#8226; MEALS AVAILABLE: {post.number}</div>
-          <div class='description'>
-            {post.description}
+      if (post.number > 0) {
+        posts.push(
+          <div style={lowerNavCss} class="ui card">
+            <div class="content">
+              <div style={header} class="header">
+                {post.name}
+              </div>
+              <div class="meta">&#8226; by {post.author} </div>
+              <div class="meta">&#8226; MEALS AVAILABLE: {post.number}</div>
+              <div class="description">{post.description}</div>
+            </div>
+            <LikeName like={this.props.like} post={post} />
           </div>
-        </div>
-        <LikeName like={this.props.like} post={post}/>
-      </div>
-      )
+        );
+      }
     }
-    return (
-      <div>
-      {posts}
-    </div>
-    );
+    return <div>{posts}</div>;
   }
 }
 
