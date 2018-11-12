75aae982bec1f32974eecdca4cbc1c70fe92a89f
omkarstpt <omkarstpt@gmail.com>
2018-11-12 16:02:01 -0600

    Added google map api

--- client/src/Map.js
+++ client/src/Map.js
@@ -1,12 +1,28 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
 
-class Map extends Component {
+class GMap extends Component {
   constructor() {
     super();
   }
   render() {
-    return <div>A MAP IS AN ABSTRACTION OF THE WORLD. NOT THE WORLD ITSELF.</div>
+    return (
+      <div>
+        <div>A MAP IS AN ABSTRACTION OF THE WORLD. NOT THE WORLD ITSELF.</div>
+        <Map
+          google={this.props.google}
+          zoom={14}
+          //visible={this.state.mapvisibility}
+        >
+          <Marker onClick={this.onMarkerClick} name={"Current location"} />
+
+          <InfoWindow onClose={this.onInfoWindowClose} />
+        </Map>
+      </div>
+    );
   }
 }
 
-export default Map;
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyCSrslYFVpdzEkb1FMefL5Q8EYIauaMiIU"
+})(GMap);
