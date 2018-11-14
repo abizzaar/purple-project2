d2bbf419d77ccc9fac98461144d8030ffb7a961c
mayli10 <mali@vassar.edu>
2018-11-14 00:30:28 -0600

    updated home page styling

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,16 +1,46 @@
 import React, {Component} from 'react';
 import Nav from './Nav.js';
 
+const background = {
+  backgroundColor: "light gray"
+}
+
 const imgContainer = {
   display: "flex",
   alignItems: "center",
   justifyContent: "center",
-  paddingTop: "70px"
+  paddingTop: "50px",
+  margin: "30px"
 }
 
-const imgStyle = {
-
+const italicText = {
+  fontStyle: "italic",
+  fontSize: "20px",
+  color: "grey"
 }
+
+const regText = {
+  fontSize: "25px"
+}
+
+const bigText = {
+  fontSize: "45px",
+  fontWeight: "bold"
+}
+
+const quoteBox = {
+}
+
+const overallContainer = {
+  display: "flex",
+  flexDirection: "row",
+  alignItems: "center",
+  justifyContent: "center",
+  marginTop: "100px",
+  paddingBottom: "100px",
+  backgroundColor: "#f7f8f9"
+}
+
 class Home extends Component {
   constructor() {
     super();
@@ -18,10 +48,18 @@ class Home extends Component {
 
   render() {
     return (
-      <div>
+      <div style={background}>
         <Nav />
-        <div style={imgContainer}>
-          <img style={imgStyle} src={"/updated-logo-2.png"} />
+        <div style={overallContainer}>
+          <div style={imgContainer}>
+            <img src={"/fixed-logo.png"} />
+          </div>
+          <div style={quoteBox}>
+            <ul style={bigText}>compañero</ul>
+            <ul style={italicText}>/kom-puh n-yair-oh/</ul>
+            <ul style={regText}>The act of sharing bread with your community to spread companionship.</ul>
+            <ul style={italicText}>Con pan, with bread.</ul>
+          </div>
         </div>
       </div>
     )
